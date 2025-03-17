941feed232430b94d7819c740022ca42f821d26a
hannovBergen <hannovonbergen98@gmail.com>
2025-03-17 15:01:12 +0100

    fixed latex not rendering in documentation

--- docs/build_documentation.sh
+++ docs/build_documentation.sh
@@ -2,4 +2,4 @@
 
 black .
 jupyter nbconvert examples/*.ipynb --to markdown --output-dir docs/converted_notebooks --execute
-pdoc -d google tangles_tot examples -o docs/
+pdoc --math -d google tangles_tot examples -o docs/
